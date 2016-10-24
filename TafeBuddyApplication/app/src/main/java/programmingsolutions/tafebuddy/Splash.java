77a27b172b0973926a68200bffe58fd96c9d943d
MatthewShinkfield <matthewshinkfield@gmail.com>
2016-10-24 13:39:01 +1030

    24/10/2016 Update
    - Splash Screen Added
    - Icons Updated
    - Beautified

---
+++ TafeBuddyApplication/app/src/main/java/programmingsolutions/tafebuddy/Splash.java
@@ -0,0 +1,27 @@
+package programmingsolutions.tafebuddy;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+/**
+ * Created by Matthew Shinkfield on 10/24/2016.
+ */
+
+public class Splash extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        Intent intent = new Intent(this, MainPage.class);
+        startActivity(intent);
+        finish();
+    }
+}
